3ac92d7bc4d632c3fe8128be5265b88ef055a520
sgc88 <sehribancheng@yahoo.com>
2017-10-12 16:34:14 -0700

    second commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,73 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+  $.ajax({
+  method: 'GET', // index
+  data:''
+  url: '/api/profile',
+  success: handleResponse,
+  error: handleError
+});
 
+function handleResponse(req, res){
+
+}
+
+function handleError(){
+
+  console.log("upss, error");
+}
+
+$.ajax({
+method: 'GET', // FOR INDEX :id
+data:''
+url: '/api/profile',
+success: handleResponse,
+error: handleError
+});
+
+function handleResponse(req, res){
+
+}
+
+function handleError(){
+
+console.log("upss, error");
+}
+
+$.ajax({
+method: 'POST',
+data: ''
+url: '/api/profile',
+success: handleResponse,
+error: handleError
+});
+
+function post(req, res){
+
+
+}
+
+// CODE GOES HERE
+// 
+// $.ajax({
+// method: 'UPDATE',
+// url: '/api/update',
+// success: handleRe,
+// error: handleError
+// });
+// function create(req, res){
+//
+// }
+
+$.ajax({
+method: 'DELETE',
+url: '/api/profile',
+success: handleResponse,
+error: handleError
+});
+function delete(req, res){
+
+
+}
 });
